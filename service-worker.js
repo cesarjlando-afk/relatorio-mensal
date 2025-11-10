ed094303490f85aa5a44381ae882dd3601a51673
cesarjlando-afk <cesarjlando@gmail.com>
2025-11-09 23:02:24 -0300

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = "rondas-preventivas-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+// Instala o Service Worker e armazena os arquivos no cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+  console.log("Service Worker instalado e arquivos armazenados no cache.");
+});
+
+// Intercepta requisições e serve do cache quando offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
